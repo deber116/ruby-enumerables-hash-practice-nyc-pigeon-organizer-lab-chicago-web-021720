53f479cc7e718966447faaa737625c41027e84c5
David Eber <deber116@gmail.com>
2020-02-07 21:41:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pp'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  new_data = data.reduce({}) do |memo, (category,characteristic_array)|
+    characteristic_array.reduce({})  do |new_memo, (characteristic,birdname_array)|
+      birdname_array.each do |name|
+        if !new_memo[name]
+          new_memo[name] = {}
+        end
+        if !new_memo[name][category]
+          new_memo[name][category] = []
+        end
+        new_memo[name][category] << characteristic.to_s
+      end
+      memo = new_memo
+      new_memo
+    end
+  #p memo["Theo"].class
+  p memo.class
+  memo
+  end
 end
+
+#need a hash of pigeons by reduce 
